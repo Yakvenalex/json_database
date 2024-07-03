82ec10e4c2b4e8d4f095799d45d89a888b16cf2f
Yakovenko Oleksii <mr.mnogo@gmail.com>
2024-07-03 13:40:27 +0300

    up funk update

--- tests/test_database.py
+++ tests/test_database.py
@@ -5,7 +5,8 @@ db = JSONDatabase('db.json')
 
 
 # Добавление новых записей
-new_data = [{"id": 1, "name": "Alice"},
+new_data = [
+    {"id": 1, "name": "Alice"},
     {"id": 2, "name": "Bob"},
     {"id": 3, "name": "Charlie"},
     {"id": 4, "name": "David"}
